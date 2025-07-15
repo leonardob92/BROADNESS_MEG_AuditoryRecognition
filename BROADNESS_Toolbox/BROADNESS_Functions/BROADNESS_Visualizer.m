03ef9b26e18d62a50629243f6c9e23928870e0e3
Leonardo Bonetti <74712035+leonardob92@users.noreply.github.com>
2025-07-15 11:50:56 +0200

    Update BROADNESS_Visualizer.m

--- BROADNESS_Toolbox/BROADNESS_Functions/BROADNESS_Visualizer.m
+++ BROADNESS_Toolbox/BROADNESS_Functions/BROADNESS_Visualizer.m
@@ -163,6 +163,10 @@ else %otherwise default is assigned
     ncomps = 1:5;
 end
 
+if ncomps > size(BROADNESS.ActivationPatterns_BrainNetworks,2) %if the requested components are more than the actual component..
+    ncomps = 1:size(BROADNESS.ActivationPatterns_BrainNetworks,2); %assigning by default the maximum number of existing components to be plotted
+end
+
 % Checking if colors are provided for PCs and experimental conditions, otherwise assigning default
 if isfield(Options,'color_PCs') %PCs
     col_comp = Options.color_PCs;
